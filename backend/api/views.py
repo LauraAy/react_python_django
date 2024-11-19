be1952037b2ee7897f662c645476d5fb685861a9
laura_ay <laura.ay1564@gmail.com>
2024-11-18 17:53:46 -0800

    Mostly working

--- backend/api/views.py
+++ backend/api/views.py
@@ -5,31 +5,29 @@ from .serializers import UserSerializer, NoteSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from .models import Note
 
-# Create your views here.
 
 class NoteListCreate(generics.ListCreateAPIView):
     serializer_class = NoteSerializer
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        user = self.reqest.user
+        user = self.request.user
         return Note.objects.filter(author=user)
 
     def perform_create(self, serializer):
         if serializer.is_valid():
             serializer.save(author=self.request.user)
-        else: 
+        else:
             print(serializer.errors)
 
+
 class NoteDelete(generics.DestroyAPIView):
-    queryset = Note.objects.all()
     serializer_class = NoteSerializer
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         user = self.request.user
         return Note.objects.filter(author=user)
-        
 
 
 class CreateUserView(generics.CreateAPIView):

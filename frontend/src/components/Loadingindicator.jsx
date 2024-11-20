cf3ac0e9746b02813bb1c92f7fd4014d143435b8
laura_ay <laura.ay1564@gmail.com>
2024-11-19 17:06:10 -0800

    correcting filename

--- frontend/src/components/Loadingindicator.jsx
+++ frontend/src/components/Loadingindicator.jsx
@@ -1,5 +1,6 @@
 import "../styles/LoadingIndicator.css"
 
+// loading indicator
 const LoadingIndicator = () => {
     return <div className="loading-container">
         <div className="loader"></div>
